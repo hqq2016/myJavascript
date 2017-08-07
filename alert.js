871ace526703d77a401331dbb5f33675c0ae15be
hqq2016 <492034331@qq.com>
2017-08-07 15:50:18 +0800

    add my alert and confirm
    add myCode for alert and confirm as mobile make for rem

---
+++ alert.js
@@ -0,0 +1,26 @@
+﻿module.exports = {
+    template: `
+        <div>
+            <div class="dialog-mask"></div>
+            <div class="dialog-box">
+                <div class="dialog-title-container">
+                    <div class="dialog-title-text">提醒</div>
+                </div>
+                <div class="dialog-content-container">
+                    <div class="dialog-item">
+                        {{warningText}}
+                    </div>
+                </div>
+                <div class="dialog-btn-container">
+                    <a href="javascript:void(0)" @click.stop.prevent='closeDialog()'>确定</a>
+                </div>
+            </div>
+        </div>
+    `,
+    props: ['warningText'],
+    methods: {
+        closeDialog: function (event) {
+            this.$emit('closeAlertDialog');
+        }
+    }
+};
---
+++ confirm.js
@@ -0,0 +1,30 @@
+﻿module.exports = {
+    template: `
+        <div>
+            <div class="dialog-mask"></div>
+            <div class="dialog-box">
+                <div class="dialog-title-container">
+                    <div class="dialog-title-text-single">提醒</div>
+                </div>
+                <div class="dialog-content-container">
+                    <div class="dialog-item">
+                        {{warningObject.text}}
+                    </div>
+                </div>
+                <div class="dialog-btn-container">
+                    <a href="javascript:void(0)" @click.stop.prevent='closeDialog()'>取消</a>
+                    <a href="javascript:void(0)" @click.stop.prevent='submit()'>确定</a>
+                </div>
+            </div>
+        </div>
+    `,
+    props: ['warningObject'],
+    methods: {
+        closeDialog: function (event) {
+            this.$emit('closeAlertDialog');
+        },
+        submit: function (event) {
+            this.$emit('submitData',[this.warningObject.type]);
+        }
+    }
+};
